d4e89bb0b962d760f84ca89185f11fbcddde76a9
Camila Apolinario <camila.cristine.a@gmail.com>
2022-01-31 10:52:01 -0300

    Adicionar arquivos de projeto.

---
+++ 1-ByteBank/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ByteBank
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        private string _cpf;
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                _cpf = value;
+                //codigo de validação
+            }
+        }
+        
+        public string Profissao { get; set; }
+       
+    }
+}
---
+++ 1-ByteBank/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+
+            segundaContaCorrente.Depositar(100);
+            primeiraContaCorrente.Depositar(200);        
+
+            Console.WriteLine("Depósito realizado na primeiraConta, saldo atual R$ " + primeiraContaCorrente._saldo);
+            Console.WriteLine("Depósito realizado na segundaConta, saldo atual R$ " + segundaContaCorrente._saldo);
+            Console.WriteLine();
+
+            primeiraContaCorrente.Sacar(100);
+            segundaContaCorrente.Sacar(50);
+
+            Console.WriteLine("Saque realizado na primeiraConta, saldo atual R$ " + primeiraContaCorrente._saldo);
+            Console.WriteLine("Saque realizado na segundaConta, saldo atual R$ " + segundaContaCorrente._saldo);
+            Console.WriteLine();
+
+            primeiraContaCorrente.Transferir(50, segundaContaCorrente);
+            Console.WriteLine("Você transferiu, para segunda conta seu saldo atual R$ " + primeiraContaCorrente._saldo);
+            Console.WriteLine();
+
+            segundaContaCorrente.Transferir(100, primeiraContaCorrente);
+            Console.WriteLine("Você transferiu, para primeiraConta seu saldo atual R$ " + segundaContaCorrente._saldo);
+            Console.WriteLine();
+
+            ContaCorrente contaCamila = new ContaCorrente();
+            contaCamila.Titular = new Cliente();
+
+            contaCamila.Titular.Nome = "Camila";
+            Console.WriteLine(contaCamila.Titular.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
